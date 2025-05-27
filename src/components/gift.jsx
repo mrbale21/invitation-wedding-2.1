8930dcc46bf92e42fab743969e6c7e99e88fa1e4
mrbell21 <hostinginweb7@gmail.com>
2025-05-27 09:07:16 +0700

    fix

--- src/components/gift.jsx
+++ src/components/gift.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import { RiFileCopyLine } from "react-icons/ri";
 import mandiri from "../assets/image/mandiri.png";
+import dana from "../assets/image/dana.png";
 
 function Gift() {
   const [copied, setCopied] = useState(false);
   const rekening = "133-00-1707665-4";
+  const nodana = "081574603275";
 
   const handleCopy = () => {
     navigator.clipboard.writeText(rekening).then(() => {
       setCopied(true);
-      setTimeout(() => setCopied(false), 2000); // Reset indikator setelah 2 detik
+      setTimeout(() => setCopied(false), 2000);
+    });
+  };
+
+  const handleCopy2 = () => {
+    navigator.clipboard.writeText(nodana).then(() => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
     });
   };
 
@@ -37,7 +46,7 @@ function Gift() {
           <div
             data-aos="zoom-in"
             data-aos-delay="200"
-            className="bg-accent border border-primary rounded-2xl shadow shadow-primary w-4/5"
+            className="bg-accent border border-primary rounded-2xl shadow shadow-primary w-4/5 mb-4"
           >
             <div className="flex flex-col justify-center items-center gap-2 text-secondary p-4 ">
               <h1 className="font-akaya-kanadaka font-semibold text-lg text-primary">
@@ -61,6 +70,43 @@ function Gift() {
               )}
               <div
                 onClick={handleCopy}
+                className="flex items-center gap-1 bg-primary text-accent p-1 px-2 rounded-sm hover:bg-primary/90 cursor-pointer"
+              >
+                <h2 className="font-raleway font-bold text-[8px]">
+                  Copy No. Rekening
+                </h2>
+                <RiFileCopyLine />
+              </div>
+            </div>
+          </div>
+
+          <div
+            data-aos="zoom-in"
+            data-aos-delay="200"
+            className="bg-accent border border-primary rounded-2xl shadow shadow-primary w-4/5"
+          >
+            <div className="flex flex-col justify-center items-center gap-2 text-secondary p-4 ">
+              <h1 className="font-akaya-kanadaka font-semibold text-lg text-primary">
+                <img src={dana} alt="mandiri" className="h-6" />
+              </h1>
+
+              <div
+                data-aos="zoom-out"
+                className="text-center w-1/2 h-0.5 bg-gray-400 "
+              />
+              <div>
+                <p className="font-cal-sans text-xs">a/n Visa</p>
+
+                <p className="font-raleway text-sm font-semibold text-primary">
+                  {nodana}
+                </p>
+              </div>
+              <div className="flex flex-col justify-between items-end"></div>
+              {copied && (
+                <span className="text-blue-500 text-[10px]">Tersalin!</span>
+              )}
+              <div
+                onClick={handleCopy2}
                 className="flex items-center gap-1 bg-primary text-accent p-1 px-2 rounded-sm hover:bg-primary/90 cursor-pointer"
               >
                 <h2 className="font-raleway font-bold text-[8px]">
